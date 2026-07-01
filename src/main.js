610c21a43958e2b8985a659151c3ad08950b8ed8
齋藤徹 <saitoutooru@saitoutoorunoMacBook-Air.local>
2026-07-02 00:29:36 +0900

    adjust menu position

--- src/main.js
+++ src/main.js
@@ -858,7 +858,7 @@ function injectGalleryCSS() {
       white-space: nowrap; pointer-events: none; user-select: none;
     }
     #topMenu {
-      position: fixed; top: 10px; right: 42px; z-index: 19000;
+      position: fixed; top: 20px; right: 42px; z-index: 19000;
       display: flex; flex-direction: column; align-items: flex-end; gap: 10px;
     }
     #topMenu button {
