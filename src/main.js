dc0627ac8a4d5164a2016eb04cd4af8a9465c127
齋藤徹 <saitoutooru@saitoutoorunoMacBook-Air.local>
2026-07-01 21:58:09 +0900

    move button to corner

--- src/main.js
+++ src/main.js
@@ -860,8 +860,8 @@ function injectGalleryCSS() {
       top: 20px;
       right: 10px;
       z-index: 30000;
-      width: 40px;
-      height: 40px;
+      width: 60px;
+      height: 60px;
       border: none;
       outline: none;
       background: url('/images/Dot_10.PNG') center center / contain no-repeat;
