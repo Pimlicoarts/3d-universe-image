b28945102ce9edd115df80fb0fc8490a62cfb843
齋藤徹 <saitoutooru@saitoutoorunoMacBook-Air.local>
2026-07-01 21:45:37 +0900

    move button to corner

--- src/main.js
+++ src/main.js
@@ -844,7 +844,7 @@ function injectGalleryCSS() {
       white-space: nowrap; pointer-events: none; user-select: none;
     }
     #topMenu {
-      position: fixed; top: 100px; right: 10px; z-index: 19000;
+      position: fixed; top: 28px; right: 42px; z-index: 19000;
       display: flex; flex-direction: column; align-items: flex-end; gap: 10px;
     }
     #topMenu button {
@@ -857,8 +857,8 @@ function injectGalleryCSS() {
     #closeContactOverlay,
     #closeWorksOverlay {
       position: fixed;
-      top: 28px;
-      right: 42px;
+      top: 10px;
+      right: 10px;
       z-index: 30000;
       width: 400px;
       height: 400px;
